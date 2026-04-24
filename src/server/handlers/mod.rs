3626d78e3e94ef590b59761fe16011e6a102ade8
rinsuki <428rinsuki+git@gmail.com>
2026-04-24 14:52:48 +0900

    chore: cargo fmt

--- src/server/handlers/mod.rs
+++ src/server/handlers/mod.rs
@@ -1,5 +1,8 @@
-use axum::http::{HeaderValue, header::{CACHE_CONTROL, VARY}};
 use crate::server::AppState;
+use axum::http::{
+    HeaderValue,
+    header::{CACHE_CONTROL, VARY},
+};
 
 pub mod api;
 
@@ -9,15 +12,21 @@ pub fn router() -> axum::Router<AppState> {
         .nest_service(
             "/assets",
             tower::ServiceBuilder::new()
-                .layer(tower_http::set_header::SetResponseHeaderLayer::if_not_present(
-                    CACHE_CONTROL,
-                    HeaderValue::from_static("public, max-age=86400, immutable"),
-                ))
-                .layer(tower_http::set_header::SetResponseHeaderLayer::if_not_present(
-                    VARY,
-                    HeaderValue::from_static("Accept-Encoding"),
-                ))
+                .layer(
+                    tower_http::set_header::SetResponseHeaderLayer::if_not_present(
+                        CACHE_CONTROL,
+                        HeaderValue::from_static("public, max-age=86400, immutable"),
+                    ),
+                )
+                .layer(
+                    tower_http::set_header::SetResponseHeaderLayer::if_not_present(
+                        VARY,
+                        HeaderValue::from_static("Accept-Encoding"),
+                    ),
+                )
                 .service(tower_http::services::ServeDir::new("dist/assets").precompressed_br()),
         )
-        .fallback_service(tower_http::services::ServeFile::new("dist/index.html").precompressed_br())
+        .fallback_service(
+            tower_http::services::ServeFile::new("dist/index.html").precompressed_br(),
+        )
 }

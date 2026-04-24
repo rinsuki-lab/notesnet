a4b0304bc9963aade3a2480d46bd7aea51e30ac6
rinsuki <428rinsuki+git@gmail.com>
2026-04-24 14:24:35 +0900

    fix: serve static files

--- src/server/handlers/mod.rs
+++ src/server/handlers/mod.rs
@@ -3,5 +3,11 @@ use crate::server::AppState;
 pub mod api;
 
 pub fn router() -> axum::Router<AppState> {
-    axum::Router::new().nest("/api", api::router())
+    axum::Router::new()
+        .nest("/api", api::router())
+        .nest_service(
+            "/assets",
+            tower_http::services::ServeDir::new("dist/assets"),
+        )
+        .fallback_service(tower_http::services::ServeFile::new("dist/index.html"))
 }

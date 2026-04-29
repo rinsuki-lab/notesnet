6f24606001eb5430be698c6fafdd71bb2563e18e
rinsuki <428rinsuki+git@gmail.com>
2026-04-29 16:37:55 +0900

    some fix

--- src/server/handlers/api/v1/graphql/query/recent_notes.ts
+++ src/server/handlers/api/v1/graphql/query/recent_notes.ts
@@ -10,18 +10,20 @@ builder.queryField("recentNotes", t =>
         type: [Note],
         resolve(query, root, args, ctx) {
             return db.query.notesTable.findMany({
-                where: {
-                    ...makeNotesWhereQueryObjectFromAuthorizedResult(ctx.authorized),
-                },
-                with: {
-                    latestRevision: {
-                        columns: {
-                            writtenAt: true,
-                            insertedAt: true,
-                            id: true,
+                ...query({
+                    where: {
+                        ...makeNotesWhereQueryObjectFromAuthorizedResult(ctx.authorized),
+                    },
+                    with: {
+                        latestRevision: {
+                            columns: {
+                                writtenAt: true,
+                                insertedAt: true,
+                                id: true,
+                            },
                         },
                     },
-                },
+                }),
                 // ULTRA HACK, remove after https://github.com/drizzle-team/drizzle-orm/issues/5047 implemented
                 orderBy: t =>
                     sql`("latestRevision"."r"->>'writtenAt')::timestamptz DESC, ("latestRevision"."r"->>'insertedAt')::timestamptz DESC, ("latestRevision"."r"->>'id')::uuid DESC`,

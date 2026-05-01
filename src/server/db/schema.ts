821b1615dd1cc22ffc85439c83308149feb842a7
rinsuki <428rinsuki+git@gmail.com>
2026-05-02 06:08:12 +0900

    chore: sync schema (wip)

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -14,11 +14,11 @@ export const personasTable = pgTable(
         id: uuid().primaryKey(),
         accountId: uuid()
             .notNull()
-            .references(() => accountsTable.id),
+            .references(() => accountsTable.id, { name: "personas_account_id_fkey", onDelete: "cascade" }),
         name: text(),
         createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
     },
-    table => [check("CHK_personas_account_id", sql`${table.name} IS NOT NULL OR ${table.id} = ${table.accountId}`)]
+    t => [check("personas_check", sql`((name IS NOT NULL) OR ((name IS NULL) AND (id = account_id)))`)]
 )
 
 export const scopesTable = pgTable("scopes", {
@@ -26,7 +26,7 @@ export const scopesTable = pgTable("scopes", {
     name: text().notNull(),
     ownerAccountId: uuid()
         .notNull()
-        .references(() => accountsTable.id),
+        .references(() => accountsTable.id, { name: "scopes_owner_account_id_fkey" }),
     createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
 })
 
@@ -34,14 +34,14 @@ export const accessTokensTable = pgTable("access_tokens", {
     id: uuid().primaryKey(),
     accountId: uuid()
         .notNull()
-        .references(() => accountsTable.id),
-    personaId: uuid().references(() => personasTable.id),
+        .references(() => accountsTable.id, { name: "access_tokens_account_id_fkey" }),
+    personaId: uuid().references(() => personasTable.id, { name: "access_tokens_persona_id_fkey" }),
     hashedToken: bytea().notNull(),
     description: text().notNull(),
     /**
      * Webインターフェース用のトークン (/api/internal/ が使える) かどうか
      */
-    isSuperToken: boolean().notNull(),
+    isSuperToken: boolean().default(false).notNull(),
     createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
     revokedAt: timestamp({ withTimezone: true }),
 })
@@ -50,10 +50,10 @@ export const scopePersonasTable = pgTable("scope_personas", {
     id: uuid().primaryKey(),
     scopeId: uuid()
         .notNull()
-        .references(() => scopesTable.id),
+        .references(() => scopesTable.id, { name: "scope_personas_scope_id_fkey", onDelete: "cascade" }),
     personaId: uuid()
         .notNull()
-        .references(() => personasTable.id),
+        .references(() => personasTable.id, { name: "scope_personas_persona_id_fkey", onDelete: "cascade" }),
 
     canReadNoteRevisions: boolean().notNull().default(false),
     canModifyNotes: boolean().notNull().default(false),
@@ -74,10 +74,10 @@ export const notesTable = pgTable("notes", {
     id: uuid().primaryKey(),
     authorPersonaId: uuid()
         .notNull()
-        .references(() => personasTable.id),
+        .references(() => personasTable.id, { name: "notes_author_persona_id_fkey" }),
     scopeId: uuid()
         .notNull()
-        .references(() => scopesTable.id),
+        .references(() => scopesTable.id, { name: "notes_scope_id_fkey" }),
     externalService: text(),
     externalId: text(),
 })
@@ -86,10 +86,10 @@ export const noteRevisionsTable = pgTable("note_revisions", {
     id: uuid().primaryKey(),
     noteId: uuid()
         .notNull()
-        .references(() => notesTable.id),
+        .references(() => notesTable.id, { name: "note_revisions_note_id_fkey", onDelete: "cascade" }),
     authorPersonaId: uuid()
         .notNull()
-        .references(() => personasTable.id),
+        .references(() => personasTable.id, { name: "note_revisions_author_persona_id_fkey" }),
     nextRevisionId: uuid(),
     summary: text(),
     /**
@@ -110,10 +110,10 @@ export const noteRelationshipsTable = pgTable("note_relationships", {
     id: uuid().primaryKey(),
     parentNoteId: uuid()
         .notNull()
-        .references(() => notesTable.id),
+        .references(() => notesTable.id, { name: "note_relationships_parent_note_id_fkey", onDelete: "cascade" }),
     childNoteId: uuid()
         .notNull()
-        .references(() => notesTable.id),
+        .references(() => notesTable.id, { name: "note_relationships_child_note_id_fkey", onDelete: "cascade" }),
     shouldListedAsChild: boolean().notNull().default(true),
     shouldListedAsParent: boolean().notNull().default(true),
     orderChild: integer(),

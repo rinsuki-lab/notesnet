c0c9dc887a6e046acaeca9815dee2107c41ee2c8
rinsuki <428rinsuki+git@gmail.com>
2026-05-05 12:03:13 +0900

    chore: cache

--- src/server/handlers/index.ts
+++ src/server/handlers/index.ts
@@ -1,16 +1,33 @@
 import { serveStatic } from "@hono/node-server/serve-static"
 import { Hono } from "hono"
+import { etag } from "hono/etag"
 
 import apiRouter from "./api/index.ts"
 
 const router = new Hono()
 router.route("/api", apiRouter)
+
+const ASSET_CACHE_CONTROL = `public, immutable, max-age=${60 * 60 * 24 * 7}, stale-while-revalidate=${60 * 60 * 24 * 30}`
+
+router.use(
+    "/assets",
+    etag({
+        retainedHeaders: ["Vary"],
+    }),
+    serveStatic({
+        root: "./dist/",
+        precompressed: true,
+        onFound(path, c) {
+            c.header("Cache-Control", ASSET_CACHE_CONTROL)
+        },
+    })
+)
+
 router.use(
     "*",
     serveStatic({
         root: "./dist/",
-        rewriteRequestPath(path) {
-            if (path.startsWith("/assets/")) return path
+        rewriteRequestPath() {
             return "/"
         },
         precompressed: true,
